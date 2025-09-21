478fedf89155b46011af447ba923d1a94add1bde
Madhanika-04 <madhanika04@gmail.com>
2025-09-22 03:16:14 +0530

    feat(core): create source file with com.madhanika.sales package and add  Main, Sale, Salesmanager java classes

---
+++ src/com/madhanika/sales/Sale.java
@@ -0,0 +1,5 @@
+package com.madhanika.sales;
+
+public class Sale {
+    
+}
---
+++ src/com/madhanika/sales/SalesManager.java
@@ -0,0 +1,6 @@
+package com.madhanika.sales;
+public class Salesmanager {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
