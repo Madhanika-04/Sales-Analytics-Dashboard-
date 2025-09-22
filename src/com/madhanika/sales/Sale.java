892d874d5142524790f031d21564dadbbf5ff716
Madhanika-04 <madhanika04@gmail.com>
2025-09-22 15:15:40 +0530

    feat(skeleton) : add class skeleton for Sale file

--- src/com/madhanika/sales/Sale.java
+++ src/com/madhanika/sales/Sale.java
@@ -1,5 +1,10 @@
 package com.madhanika.sales;
-
+//import necessary package (datetime)
 public class Sale {
-    
+
+    // declare variables(private)
+    public Sale(){
+    //constructor with paramater //
+    }
+    // getter function for all variables declared (to access private variable)
 }
