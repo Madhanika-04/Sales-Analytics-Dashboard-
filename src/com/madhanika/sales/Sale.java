6725327bc767f72e1d40f5f0a40f00997bf1ba9e
Madhanika-04 <madhanika04@gmail.com>
2025-09-22 20:15:14 +0530

    feat: add code for sale.java (variable declaration, constructor, getter function)

--- src/com/madhanika/sales/Sale.java
+++ src/com/madhanika/sales/Sale.java
@@ -1,10 +1,36 @@
 package com.madhanika.sales;
-//import necessary package (datetime)
+import java.time.LocalDate;
 public class Sale {
-
-    // declare variables(private)
-    public Sale(){
-    //constructor with paramater //
+//Declaration
+private String productName;
+private int Qty;
+private Double priceperunit ;
+private String Region;
+private LocalDate date;
+    //parameterized constructor
+    public Sale(String Pname, int Qty, Double price, String region, LocalDate date){
+    this.productName = Pname;
+    this.Qty = Qty;
+    this.priceperunit = price;
+    this.Region = region;
+    this.date = date;
     }
     // getter function for all variables declared (to access private variable)
+    public String getProductName(){
+        return productName;
+    } public String getregion(){
+        return Region;
+    } public  int getquantity(){
+        return Qty;
+    }
+    public Double getPriceperunit(){
+        return priceperunit;
+    }
+    public LocalDate getDate(){
+        return date;
+    }
+    public  Double getTotalPrice(){
+        return Qty * priceperunit;
+    }
+
 }
